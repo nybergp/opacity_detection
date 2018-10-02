526c151efa109e8fff68a30714c604615893dbbe
Per Nyberg <nyberg.per@outlook.com>
2018-10-02 16:33:52 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,62 @@
+from prep_data import *
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 3, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(3, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+########################################################################
+# Loss function and optimizer
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+########################################################################
+# Train the network
+
+if __name__ == '__main__':
+    for epoch in range(1):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data['image'].type('torch.FloatTensor'), data['pneumonia']
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print loss & iteration
+            running_loss += loss.item()
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
---
+++ prep_data.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from skimage import io, transform
+import os
+import torch
+import numpy as np
+
+class PneumoniaDataset(Dataset):
+    """Pneumonia opacity dataset"""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.pneumonia_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.pneumonia_frame)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir,
+                                self.pneumonia_frame.iloc[idx, 0] + '.jpg')
+        image = io.imread(img_name, as_gray=True)
+        image = np.expand_dims(image, 2)
+        pneumonia = self.pneumonia_frame.iloc[idx, 1:].values
+        #pneumonia = pneumonia.astype('float')#.reshape(1, 1)
+
+        sample = {'image': image, 'pneumonia': pneumonia[0]}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        image, pneumonia = sample['image'], sample['pneumonia']
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top: top + new_h,
+                      left: left + new_w]
+
+        return {'image': image, 'pneumonia': pneumonia}
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, pneumonia = sample['image'], sample['pneumonia']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image),
+                'pneumonia': pneumonia}
+
+trainset = PneumoniaDataset(csv_file='datasets/png_labels.csv',
+                                           root_dir='datasets/stage_1_small_png',
+                                           transform=transforms.Compose([
+                                           RandomCrop(32), ToTensor()])
+                            )
+
+trainloader = DataLoader(trainset, batch_size=4,
+                        shuffle=True, num_workers=4)
+
+classes = ('0', '1')
